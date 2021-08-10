a02708962cb558689d0d45327d935f860b62dfef
Elsayed93 <elsayedelbeshry119@gmail.com>
2021-08-10 03:06:09 +0200

    build local server and run it

--- server.js
+++ server.js
@@ -2,18 +2,39 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require("express");
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require("body-parser");
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+const port = 8080;
+app.listen(port, function () {
+  console.log(`server run at: http://localhost:${port}/`);
+});
+
+// get route
+app.get("/get-all-data", function (req, res) {
+  res.send(projectData);
+});
+
+// post route
+app.post("sava-data", function (req, res) {
+  console.log(req.body);
+  projectData.temperature = req.body.temperature;
+  projectData.date = req.body.date;
+  projectData.userResponse = req.body.userResponse;
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,7 @@
 /* Global Variables */
 
+const baseURL = 'api.openweathermap.org/data/2.5/weather?zip=';
+const appId  = 'b6e3287c771945b3e7251f142c038309';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
